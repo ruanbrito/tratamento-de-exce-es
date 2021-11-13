6fe504afa99976c9e31a3baaf919596e9f10126d
ruanbrito <rpablobrito@gmail.com>
2021-11-12 22:55:54 -0300

    versão boa no tratamento de exceções, utilizando uma classe propria de exceções e delegando as classes o tratamento

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -6,44 +6,47 @@ import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Scanner scn = new Scanner(System.in);
-
-		System.out.print("Room number: ");
-		int roomNumber = scn.nextInt();
-		System.out.print("Check-in date (dd/MM/yyyy): ");
-		Date checkIn = sdf.parse(scn.next());
-		System.out.print("Check-out date (dd/MM/yyyy): ");
-		Date checkOut = sdf.parse(scn.next());
-		System.out.println();
-		if (!checkOut.after(checkIn)) {
-			System.out.println("Error in reservation: Check-out date must be after check-in date");
-		} else {
+		
+		try {
+			System.out.print("Room number: ");
+			int roomNumber = scn.nextInt();
+			System.out.print("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = sdf.parse(scn.next());
+			System.out.print("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = sdf.parse(scn.next());
+			System.out.println();
+		
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-
+		
 			System.out.println();
 			System.out.println("Enter data to update the reservation:");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
 			checkIn = sdf.parse(scn.next());
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(scn.next());
-
+		
+			reservation.updateDates(checkIn, checkOut);
+		
+			System.out.println("Reservation: " + reservation);
 			
-			String error =	reservation.updateDates(checkIn, checkOut);
-			
-			if(error != null) {
-				System.out.println("Error in reservation: " + error);
-			}else {
-				System.out.println("Reservation: " + reservation);
-			}
+		} catch (ParseException e) {
+			System.out.println("Invalid date format!");			
+		} catch (DomainException e) { //exceções personalizadas, que são esperadas.
+			System.out.println("Error reservation: " + e.getMessage());
+		} catch (RuntimeException e) { //exceções que nao estão sendo esperadas no programa e que podem aparecer
+			System.out.println("Unexpected error");
 		}
-
+		
+		
 		scn.close();
 	}
 
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
@@ -12,7 +14,15 @@ public class Reservation {
 
 	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+		if (!checkOut.after(checkIn)) {
+			// metodo para lançar a exceção
+			// throw new IllegalArgumentException("Check-out date must be after check-in
+			// date");
+
+			// utilizando a classe para tratamento das exceções
+			throw new DomainException("Check-out date must be after check-indate");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -40,19 +50,27 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException {
 		Date now = new Date();
 
 		if (checkIn.before(now) || checkOut.before(now)) {
-			return "Check-out date must be future dates";
+			// metodo para lançar a exceção
+			// throw new IllegalArgumentException("Check-out date must be future dates");
+
+			// utilizando a classe para tratamento das exceções
+			throw new DomainException("Check-out date must be future dates");
 		}
-		if ((!checkOut.after(checkIn))) {
-			return "Check-out date must be after check-in date";
+		if (!checkOut.after(checkIn)) {
+			// metodo para lançar a exceção
+			// throw new IllegalArgumentException("Check-out date must be after check-in
+			// date");
+
+			// utilizando a classe para tratamento das exceções
+			throw new DomainException("Check-out date must be after check-indate");
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 
-		return null;
 	}
 
 	@Override
